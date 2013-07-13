94bb77b16376610aaa19c9184375fe02aee02106
Dmitry <crutch.kov@gmial.com>
2013-07-13 12:38:31 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var index = fs.readdurSync("/");
 //var index = fs.readFileSync("index.html");
-var index = "index.html";
-var buffer = new Buffer(index.length);
-buffer.write(index);
+//var index = "index.html";
+//var buffer = new Buffer(index.length);
+//buffer.write(index);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
