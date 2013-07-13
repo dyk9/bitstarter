3703efa32938a1ba37d3a02d37f11a786cd2acb2
Dmitry <crutch.kov@gmial.com>
2013-07-13 12:04:35 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
-//var index = fs.readFileSync("index.html");
-var index = "index.html";
+var index = fs.readdirSync(".");
+///var index = fs.readFileSync("index.html");
+//var index = "index.html";
 var buffer = new Buffer(index.length);
 buffer.write(index);
 
