83de34e7c645cf503af8243d6c739e0f1f579e65
Dmitry <crutch.kov@gmial.com>
2013-07-13 12:51:35 +0000

    Updated web.js with fs.readFileSync and output with buffer

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var index = fs.readFileSync("index.html");
-var buffer = new Buffer(index.length);
-buffer.write(index);
+var buffer = new Buffer(fs.readFileSync("index.html","utf-8"));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
