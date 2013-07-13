46c6f41e28fc7fb9f98703183786c148e1454055
Dmitry <crutch.kov@gmial.com>
2013-07-13 11:44:56 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var buffer = new Buffer(fs.readFileSync("index.html")
+var buffer = new Buffer(fs.readFileSync("index.html"));
 
 var app = express.createServer(express.logger());
 
