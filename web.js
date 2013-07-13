d61f7b79b0f5017c21edcaade491cfa2a84cad84
Dmitry <crutch.kov@gmial.com>
2013-07-13 12:46:55 +0000

    Updated web.js with fs.readFileSync and output with buffer

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var index = fs.readFileSync("index.html");
-//var index = "index.html";
-//var buffer = new Buffer(index.length);
-//buffer.write(index);
+var buffer = new Buffer(index.length);
+buffer.write(index);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
