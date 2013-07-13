9dccf8cca95a6572ebbf94767187a3da9fa8e5e6
Dmitry <crutch.kov@gmial.com>
2013-07-13 12:44:21 +0000

    Updated web.js with fs.readFileSync

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var index = fs.readdirSync("/");
-//var index = fs.readFileSync("index.html");
+var index = fs.readFileSync("index.html");
 //var index = "index.html";
 //var buffer = new Buffer(index.length);
 //buffer.write(index);
